97cbce7b1d53f25ced767bd9fcb7585128263929
James H. Edwards <james.howe.edwards@gmail.com>
2014-01-07 15:51:53 +0800

    update index.htm and place passage data in external file

---
+++ passage-data.js
@@ -0,0 +1,14 @@
+var passageData = [
+	{
+	  ID: 'lzzmw4',
+	  origin: 'Lie Zi',
+		title: 'King Mu of Zhou.4',
+		trans: "In the southern corner of the far west there is a kingdom.<br> I don't know what its borders touch, it is called the kingdom of ancient grass.<br>The vapors of Yin and Yang do not mix there, so winter and summer loose their distinction.<br> The sun and moon do not shine, so morning and night lose their distinction.<br> The people there do not eat, do not wear clothes, and frequently sleep.<br> They wake up once every five weeks, taking their dream lives to be real and their waking lives to be false.<br><br> There is a country which is said to lie in the center of the four oceans, that straddles the north and south of the Yellow River,<br> and extends from Dai mountain to the east and west for more than ten thousand li.<br> Yin and Yang are clearly distinguised, therefore there is winter and summer.<br> Dark and bright have been divided, therefore there is night and day.<br> Of the people there, some and wise and some are foolish.<br> The ten thousand things all breed and multiply, talent and art is plentiful.<br> The ruler and the minister rule together, while law and ritual mutually support the state.<br> The way that they discuss action cannot be called scheming.<br> They take waking to be real and dreaming to be false."
+	},
+	{
+  	ID: 'gzxs1',
+  	origin: 'Guan Zi',
+		title: 'Shape.1',
+		trans: 'Mountains are high yet do not collapse, therefore they obtain the goat sacrifice.<br> The abyss is deep yet never exhausted, therefore it may obtain the heaviest jade.<br> Heaven does not change its principles, earth does not change its patterns.<br> Spring, summer, autumn and winter do not alter their transitions. In the past and today it is the same.<br> Only when the dragon is in water is its spirit manifest.<br> The tiger and leopard must be in dark valleys for their fearsomeness to manifest.<br> Wind and rain have no village, yet anger and hatred never meet them.<br> Respected people must give orders, humble people must forget their poverty.<br> Long life, early death, wealth and poverty, these things have no cause(?)'
+  }
+];
