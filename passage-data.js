f13925504bf37165691a212d6fe2ea25d9dcef26
James H. Edwards <james.howe.edwards@gmail.com>
2014-01-21 11:53:53 +0800

    restructure for single page catalog

--- passage-data.js
+++ passage-data.js
@@ -1,14 +1,56 @@
 var passageData = [
 	{
-	  ID: 'lzzmw4',
-	  origin: 'Lie Zi',
-		title: 'King Mu of Zhou.4',
-		trans: "In the southern corner of the far west there is a kingdom.<br> I don't know what its borders touch, it is called the kingdom of ancient grass.<br>The vapors of Yin and Yang do not mix there, so winter and summer loose their distinction.<br> The sun and moon do not shine, so morning and night lose their distinction.<br> The people there do not eat, do not wear clothes, and frequently sleep.<br> They wake up once every five weeks, taking their dream lives to be real and their waking lives to be false.<br><br> There is a country which is said to lie in the center of the four oceans, that straddles the north and south of the Yellow River,<br> and extends from Dai mountain to the east and west for more than ten thousand li.<br> Yin and Yang are clearly distinguised, therefore there is winter and summer.<br> Dark and bright have been divided, therefore there is night and day.<br> Of the people there, some and wise and some are foolish.<br> The ten thousand things all breed and multiply, talent and art is plentiful.<br> The ruler and the minister rule together, while law and ritual mutually support the state.<br> The way that they discuss action cannot be called scheming.<br> They take waking to be real and dreaming to be false."
-	},
+    text: 'Lie Zi',
+    info: 'Lie Zi is an unusual Daoist text from the Warring States period.',
+    passages: [
+    {
+	    ID: 'lzzmw4',
+		  title: 'King Mu of Zhou.4',
+		  trans: "In the southern corner of the far west there is a kingdom.<br> I don't know what its borders touch, it is called the kingdom of ancient grass.<br>The vapors of Yin and Yang do not mix there, so winter and summer loose their distinction.<br> The sun and moon do not shine, so morning and night lose their distinction.<br> The people there do not eat, do not wear clothes, and frequently sleep.<br> They wake up once every five weeks, taking their dream lives to be real and their waking lives to be false.<br><br> There is a country which is said to lie in the center of the four oceans, that straddles the north and south of the Yellow River,<br> and extends from Dai mountain to the east and west for more than ten thousand li.<br> Yin and Yang are clearly distinguised, therefore there is winter and summer.<br> Dark and bright have been divided, therefore there is night and day.<br> Of the people there, some and wise and some are foolish.<br> The ten thousand things all breed and multiply, talent and art is plentiful.<br> The ruler and the minister rule together, while law and ritual mutually support the state.<br> The way that they discuss action cannot be called scheming.<br> They take waking to be real and dreaming to be false."
+	  }
+    ]
+  },
 	{
-  	ID: 'gzxs1',
-  	origin: 'Guan Zi',
-		title: 'Shape.1',
-		trans: 'Mountains are high yet do not collapse, therefore they obtain the goat sacrifice.<br> The abyss is deep yet never exhausted, therefore it may obtain the heaviest jade.<br> Heaven does not change its principles, earth does not change its patterns.<br> Spring, summer, autumn and winter do not alter their transitions. In the past and today it is the same.<br> Only when the dragon is in water is its spirit manifest.<br> The tiger and leopard must be in dark valleys for their fearsomeness to manifest.<br> Wind and rain have no village, yet anger and hatred never meet them.<br> Respected people must give orders, humble people must forget their poverty.<br> Long life, early death, wealth and poverty, these things have no cause(?)'
+    text: 'Guan Zi',
+    info: 'The Guan Zi is a collection of writings supposedly authored by the great statesman of the Spring and Autumn peiod.',
+    passages: [
+    {
+      ID: 'gzxs1',
+      title: 'Shape.1',
+      trans: 'Mountains are high yet do not collapse, therefore they obtain the goat sacrifice.<br> The abyss is deep yet never exhausted, therefore it may obtain the heaviest jade.<br> Heaven does not change its principles, earth does not change its patterns.<br> Spring, summer, autumn and winter do not alter their transitions. In the past and today it is the same.<br> Only when the dragon is in water is its spirit manifest.<br> The tiger and leopard must be in dark valleys for their fearsomeness to manifest.<br> Wind and rain have no village, yet anger and hatred never meet them.<br> Respected people must give orders, humble people must forget their poverty.<br> Long life, early death, wealth and poverty, these things have no cause(?)'  
+    }
+    ]
+  },
+  {
+    text: 'Chu Ci',
+    info: 'The classic collection of poetry from ancient Hunan.',
+    passsages: [
+    {
+      ID: 'fisherman',
+      title: 'The Fisherman',
+      trans: 'After Qu Yuan had been dispatched from his official position, he was wandering on the edge of a river.<br> He walked and sang through the marshes and the narrow paths.<br>His appearance was worn out an haggard like a dried up withered stick.<br>A fisherman saw him and asked:<br>Arent you the lord of a village? What on earth has brought you here?<br>Qu Yuan said: "this generation is muddy, I alone am clear,<br>The masses are all drunk, I alone am sober. This is why I have been exiled."<br>The fisherman replied: "Sages do not get caught up in things, instead they are able to rearrange things.<br>The generation is dirty, so play in the mud and make waves. Everyone is drunk, so eat and drink with them.<br> Why would you use deep thoughts to elevate yourself, and thereby exile yourself?"<br>Qu Yuan replied: "I have heard that a man who has just washed his head must clean his hat, and a man who has just bathed must air out his robes.<br> Could I take my clean body and go recieve the filth of the world?<br>I would rather go to the flowing Xiang and bury myself in the belly of a river fish.<br>Can I take my pure white mind and cover it in the dirt of the vulgar masses?<br>The fisherman smiled, and then he laughed. He picked up his oar and paddled away.<br> As he was leaving he began to sing:"when the Canglang waters are clean, I can wash my hat there.<br> When the Canglang waters are dirty, I can wash my feet there."<br> He was never seen again.'
+    }
+    ]
+  },
+  {
+    text: 'Lun Heng',
+    info: 'A Han dynasty collection of writings in the skeptic tradition.',
+    passages: [
+    {
+      ID: 'lhbw',
+      title: 'Divination and Prophesy',
+      trans: 'The masses believe in divination, calling divination \'asking heaven\' and \'asking earth\'.<br> They say that grass stalks have a spirit and turtle shells have a soul.<br> They believe in omens, fate, and retribution.<br> Therefore they give up consulting with each other and go divining, they forget about \'should\' and \'should not\' and believe in \'auspicious\' and \'inauspicious\'.<br> This idea is called the announcement of interrogating heaven and earth and the true spirit of turtle shells and grass stalks.<br> If we are going to actually discuss things, divination does not ask of heaven and earth and stalks and shells have no need for a spirit.<br> If we have spirits and ghosts, asking heaven and earth, well, that\'s what the masses and the confucianists say.'
+    }
+    ]    
+  },
+  {
+    text: 'Huai Nan Zi',
+    passages: [
+    {
+      ID: 'hnzyd',
+      title: 'Original Dao',
+      trans: 'As for the Dao, it covers heaven and carries earth.<br> It expands the four directions and opens up the eight extremities.<br> Its height cannot be examined, its depth cannot be measured.<br> It gives only to that which is formless.<br> At its origin it bubbles up vigorously, spreading everywhere and slowly filling up [the universe?].<br> Turbulent and smooth; turbid, it slowly becomes clear.<br> Therefore grow it and it fills heaven, spread it out and it covers the four seas.<br>'
+    }
+    ]    
   }
 ];
